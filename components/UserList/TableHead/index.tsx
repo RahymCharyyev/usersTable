3ff138b43fea76c386c3f45772e56ebf97ea8e59
RahymCharyev <r569837@gmail.com>
2023-09-11 12:05:23 +0500

    ✨feature: Fixed image to next image component

--- components/UserList/TableHead/index.tsx
+++ components/UserList/TableHead/index.tsx
@@ -4,18 +4,20 @@ const headers = ['ID', 'Given Name', 'Family Name', 'Email'];
 const TableHead = () => {
   return (
     <thead className='sticky top-0 z-10'>
-      {headers.map((header, index) => (
-        <th
-          key={`header-${index}`}
-          className={`${
-            index === 0
-              ? 'sticky left-0 bg-header-bg text-center text-xs font-medium text-white uppercase whitespace-nowrap'
-              : 'bg-header-bg text-center text-xs font-medium text-white uppercase whitespace-nowrap'
-          } px-4 py-1 border border-gray-400`}
-        >
-          {header}
-        </th>
-      ))}
+      <tr>
+        {headers.map((header, index) => (
+          <th
+            key={`header-${index}`}
+            className={`${
+              index === 0
+                ? 'sticky left-0 bg-header-bg text-center text-xs font-medium text-white uppercase whitespace-nowrap'
+                : 'bg-header-bg text-center text-xs font-medium text-white uppercase whitespace-nowrap'
+            } px-4 py-1 border border-gray-400`}
+          >
+            {header}
+          </th>
+        ))}
+      </tr>
     </thead>
   );
 };
